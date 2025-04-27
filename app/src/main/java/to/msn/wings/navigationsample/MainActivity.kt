bd26b05e34ab808cfb7ce8901ebf22ab8549661f
marjunstudio <uebujie@gmail.com>
2025-04-27 10:45:09 +0900

    first commit

---
+++ app/src/main/java/to/msn/wings/navigationsample/MainActivity.kt
@@ -0,0 +1,48 @@
+package to.msn.wings.navigationsample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import to.msn.wings.navigationsample.ui.theme.NavigationSampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavigationSampleTheme {
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "firstscreen") {
+        composable("firstscreen") {
+            FirstScreen {
+                navController.navigate("secondscreen")
+            }
+        }
+        composable(route = "secondscreen"){
+            SecondScreen {
+                navController.navigate("firstscreen")
+            }
+        }
+    }
+}
